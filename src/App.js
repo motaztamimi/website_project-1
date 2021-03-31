d203223a727b41f464afb775027e29c99807574e
motaztamimi <74739600+motaztamimi@users.noreply.github.com>
2021-03-31 20:28:54 +0300

    slideShowEdit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Header from "./components/Header"
+import SlideShow from "./components/SlideShow"
 function App() {
   return (
     <div className="App">
       <Header/>
+      <SlideShow/>
     </div>
   );
 }
---
+++ src/components/imgeData.jsx
@@ -0,0 +1,13 @@
+
+
+export const imgeData=[
+
+{
+    image:'"../imges/img1.jpg"'
+},
+{
+    image:'"../imges/img2.jpg"'
+},{
+    image:'"../imges/img3.jpg"'
+},
+]

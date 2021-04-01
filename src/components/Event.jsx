709a0e968b17788a36b32a93a1216100e65e477c
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-04-01 16:23:50 +0300

    updated css

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import img from '../imges/event1.jpg'
 const Event = ({event}) => {
     return (
      
         <div className="Event">
         <h6>{event.date}</h6>
-            <img className="eventImg" src={img} alt=""/>
             <img className="eventimg" src={event.img} alt=""/>
 
             <h3>{event.CardTitle}</h3>

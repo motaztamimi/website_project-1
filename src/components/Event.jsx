2eca5d656804e2ebc5b62c6af52c3ba265c996b1
motaztamimi <74739600+motaztamimi@users.noreply.github.com>
2021-04-01 15:30:28 +0300

    create Event container

---
+++ src/components/Event.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import img from '../imges/event1.jpg'
+const Event = ({event}) => {
+    return (
+     
+        <div className="Event">
+        <h6>{event.date}</h6>
+            <img className="eventimg" src={img} alt=""/>
+            <h3>{event.CardTitle}</h3>
+            <h5>{event.body}</h5>
+        </div>
+      
+    )
+}
+
+export default Event

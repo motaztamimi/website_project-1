605ead1500ca7d0fd590bd2b97821570fcad5ccc
motaztamimi <74739600+motaztamimi@users.noreply.github.com>
2021-04-01 16:16:32 +0300

    update event photo

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -5,7 +5,7 @@ const Event = ({event}) => {
      
         <div className="Event">
         <h6>{event.date}</h6>
-            <img className="eventimg" src={img} alt=""/>
+            <img className="eventimg" src={event.img} alt=""/>
             <h3>{event.CardTitle}</h3>
             <h5>{event.body}</h5>
         </div>

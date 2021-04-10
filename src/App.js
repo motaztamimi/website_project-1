ababd8352ffee4e4a2eb56431bf8108a35522782
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-04-10 20:43:00 +0300

    Added React-route-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 
 import './App.css';
+import Fotter from './components/Fotter';
 import HomePage from './components/HomePage';
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import About from './components/About';
 
 
 function App() {
 
   return (
     <div className="App">
-        <HomePage/>
+      <Router>
+        <Header/>
+          <Switch>
+            <Route exact path='/'>
+             <HomePage/>
+             </Route>
+             <Route exact path='/about'>
+             <About/>
+             </Route>
+          </Switch> 
+        <Fotter/>
+        </Router>
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import SlideShow from "./SlideShow"
+
+
+const About = ()=>{
+
+
+    return (
+        <div>
+            <SlideShow/>
+        </div>
+    );
+
+}
+
+
+export default About;

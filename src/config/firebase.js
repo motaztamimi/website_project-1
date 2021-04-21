2e0cb511e5a787663676524d9633004d0bdc26c2
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-04-22 01:27:43 +0300

    added firebase.js

---
+++ src/config/firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+  
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCkqQpRXlJlbE3Oqdetn4rNU34gOgJ0jbQ",
+    authDomain: "mental-health-center-27530.firebaseapp.com",
+    projectId: "mental-health-center-27530",
+    storageBucket: "mental-health-center-27530.appspot.com",
+    messagingSenderId: "1082760898572",
+    appId: "1:1082760898572:web:37b8e80b562767d8336629",
+    measurementId: "G-KT6D6Y4GSL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  const dataBase = firebase.firestore();
+  const storage = firebase.storage();
+
+  export { dataBase , storage };

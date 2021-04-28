a75f913b35a6cc09b13da9da7af9121cb20ee4fd
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-04-28 13:07:36 +0300

    ADminEditGalleryUpdated

--- src/components/AdminEditGallery.jsx
+++ src/components/AdminEditGallery.jsx
@@ -3,7 +3,58 @@ import '../style/AdminEditGallery.css';
 const AdminEditGallery = () => {
   return (
     <div className='AdminGallery'>
-      <h1>הגלאריה שלך </h1>
+      <h1 className='GalleryHeading'>הגלאריה שלך </h1>
+      <div className='AdminGalleryContent'>
+        <div className='AdminGalleryImg'>
+          <input type='button' value='X' />
+          <img src={`${process.env.PUBLIC_URL}/imges/event3.jpg`} alt='' />
+        </div>
+        <div className='AdminGalleryImg'>
+          <input type='button' value='X' />
+
+          <img src={`${process.env.PUBLIC_URL}/imges/event3.jpg`} alt='' />
+        </div>
+        <div className='AdminGalleryImg'>
+          <input type='button' value='X' />
+
+          <img src={`${process.env.PUBLIC_URL}/imges/event3.jpg`} alt='' />
+        </div>
+        <div className='AdminGalleryImg'>
+          <input type='button' value='X' />
+
+          <img src={`${process.env.PUBLIC_URL}/imges/event3.jpg`} alt='' />
+        </div>
+        <div className='AdminGalleryImg'>
+          <input type='button' value='X' />
+
+          <img src={`${process.env.PUBLIC_URL}/imges/event3.jpg`} alt='' />
+        </div>
+        <div className='AdminGalleryImg'>
+          <input type='button' value='X' />
+
+          <img src={`${process.env.PUBLIC_URL}/imges/event3.jpg`} alt='' />
+        </div>
+        <div className='AdminGalleryImg'>
+          <input type='button' value='X' />
+
+          <img src={`${process.env.PUBLIC_URL}/imges/event3.jpg`} alt='' />
+        </div>
+        <div className='AdminGalleryImg'>
+          <input type='button' value='X' />
+
+          <img src={`${process.env.PUBLIC_URL}/imges/event3.jpg`} alt='' />
+        </div>
+        <div className='AdminGalleryImg'>
+          <input type='button' value='X' />
+
+          <img src={`${process.env.PUBLIC_URL}/imges/event3.jpg`} alt='' />
+        </div>
+        <div className='AdminGalleryImg'>
+          <input type='button' value='X' />
+
+          <img src={`${process.env.PUBLIC_URL}/imges/event3.jpg`} alt='' />
+        </div>
+      </div>
     </div>
   );
 };

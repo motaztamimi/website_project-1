8c5091c2d06ca6bfeb485a5e6befcde3320fab96
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-04-28 12:02:47 +0300

    started working on the Gallery Page

--- src/components/AdminEditGallery.jsx
+++ src/components/AdminEditGallery.jsx
@@ -1,7 +1,9 @@
+import '../style/AdminEditGallery.css';
+
 const AdminEditGallery = () => {
   return (
-    <div>
-      <h1>HelloWorld</h1>
+    <div className='AdminGallery'>
+      <h1>הגלאריה שלך </h1>
     </div>
   );
 };

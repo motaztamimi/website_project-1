7866af973b7014d11a45a459c764046ab8f5cd7e
motaztamimi <74739600+motaztamimi@users.noreply.github.com>
2021-05-03 21:27:03 +0300

    update the Centre to be responsive

--- src/components/Centers.jsx
+++ src/components/Centers.jsx
@@ -7,54 +7,59 @@ const Centers = ({ Cent }) => {
     firstone: 1,
     secondone: 2,
   });
-  let cen;
+  let cen = Cent;
+  let first = Centerr.firstone;
+  let second = Centerr.secondone;
+
   function getCenter() {
     cen = Cent;
     if (window.innerWidth < 800) {
-      let mot;
-      if (Centerr.secondone == 1) {
-        console.log('im ad');
-        mot = cen.filter((act) => act.id == Centerr.firstone);
-        let z = cen.filter((act) => act.id == Centerr.secondone);
-        mot = [...mot, z];
-        cen = mot;
-      } else {
-        mot = cen.filter(
-          (act) => act.id == Centerr.secondone || act.id == Centerr.firstone
-        );
-      }
-      console.log(mot);
-      return mot;
+      let lastindex;
+
+      lastindex = cen.filter((act) => act.id == Centerr.firstone);
+      let firstindexinlast = cen.filter((act) => act.id == Centerr.secondone);
+
+      lastindex = [...lastindex, ...firstindexinlast];
+
+      return lastindex;
     }
     return cen;
   }
   function plusCenter(n) {
-    setCenteer({
-      firstone: Centerr.secondone,
-      secondone: Centerr.secondone + n,
-    });
-
-    if (Centerr.secondone == Object.keys(Cent).length) {
-      console.log(
-        'imhere first ' + Centerr.firstone + 'secons' + Centerr.secondone
-      );
-      setCenteer({ firstone: Centerr.firstone, secondone: 1 });
+    first = Centerr.firstone + n;
+    second = Centerr.secondone + n;
+    if (first > Object.keys(cen).length) {
+      first = 1;
     }
+    if (second > Object.keys(cen).length) {
+      second = 1;
+    }
+    if (first <= 0) {
+      first = Object.keys(cen).length;
+    }
+    if (second <= 0) {
+      second = Object.keys(cen).length;
+    }
+    setCenteer({ firstone: first, secondone: second });
+    getCenter();
 
     return n;
   }
 
   function minousCenter(n) {
-    return n;
+    first = Centerr.firstone + n;
+    second = Centerr.secondone + n;
+    if (first > Object.keys(cen).length) {
+      first = 1;
+    }
+    if (second > Object.keys(cen).length) {
+      second = 1;
+    }
+
+    setCenteer({ firstone: first, secondone: second });
+    getCenter();
   }
-  useEffect(() => {
-    console.log(
-      'getcenter before first' +
-        Centerr.firstone +
-        ' before second ' +
-        Centerr.secondone
-    );
-  });
+
   return (
     <section className='Center_Container'>
       <h2> המרכז שלנו</h2>
@@ -63,10 +68,10 @@ const Centers = ({ Cent }) => {
           return <Center key={cen.id} CENTER={cen} />;
         })}
       </section>
-      <button className='prev_Center' onClick={() => minousCenter(-1)}>
+      <button className='prev_Center' onClick={() => minousCenter(1)}>
         &#10095;
       </button>
-      <button className='next_Center' onClick={() => plusCenter(1)}>
+      <button className='next_Center' onClick={() => plusCenter(-1)}>
         &#10094;
       </button>
     </section>

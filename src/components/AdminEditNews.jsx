1d390cd9cbc82d3ee54f573c15389e5c658d1106
motaztamimi <74739600+motaztamimi@users.noreply.github.com>
2021-05-16 22:30:30 +0300

    update AdminEditNews

--- src/components/AdminEditNews.jsx
+++ src/components/AdminEditNews.jsx
@@ -1,20 +1,27 @@
 /** @format */
 import { useHistory } from 'react-router-dom';
 import useDataBase from '../hooks/useDataBase';
-
+import Events from '../components/Events';
+import '../style/AdminEditNews.css';
 const AdminEditNews = () => {
   let history = useHistory();
   const News = useDataBase('News');
 
   console.log(News);
-
   return (
-    <button
-      onClick={() => {
-        history.push('/Admin/EditNews/AddNews');
-      }}>
-      Add news
-    </button>
+    <div>
+      <div className='AddNewsButtonDiv'>
+        <button
+          onClick={() => {
+            history.push('/Admin/EditNews/AddNews');
+          }}
+          className='AddNewsButton'>
+          Add news
+        </button>
+      </div>
+
+      {<Events news={News.docs} />}
+    </div>
   );
 };
 

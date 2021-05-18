b451160e35d34b08899b14e52aadc1f22dd7560f
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-05-18 17:21:10 +0300

    edited hoemPage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -114,53 +114,6 @@ const HomePage = () => {
     },
   ];
 
-  const news = [
-    {
-      id: 1,
-      title: 'גאווה ישראלית – שנה שלישית ברציפות',
-      brife: ' שיבא ב-TOP 10 של צמרת בתי החולים הטובים ביותר בעולם',
-      img: `${process.env.PUBLIC_URL}/imges/newsimg2.jpg`,
-    },
-    {
-      id: 2,
-      title: 'גאווה ישראלית – שנה שלישית ברציפות',
-      brife: ' שיבא ב-TOP 10 של צמרת בתי החולים הטובים ביותר בעולם',
-      img: `${process.env.PUBLIC_URL}/imges/newsimg3.jpg`,
-    },
-    {
-      id: 3,
-      title: 'גאווה ישראלית – שנה שלישית ברציפות',
-      brife: ' שיבא ב-TOP 10 של צמרת בתי החולים הטובים ביותר בעולם',
-      img: `${process.env.PUBLIC_URL}/imges/News1.jpeg`,
-    },
-  ];
-
-  const events = [
-    {
-      id: '1',
-      CardTitle: 'firstone',
-      CardSubtitle: 'first',
-      date: '2thFeb',
-      body: 'הפלונטר והבחירות לנשיאות: האם יו"ר הכנסת לוין ייכנס לנעלי ריבלין? ',
-      img: `${process.env.PUBLIC_URL}/imges/event1.jpg`,
-    },
-    {
-      id: '2',
-      CardTitle: 'Secondone',
-      CardSubtitle: 'Second',
-      date: '3thFeb',
-      body: 'הפלונטר והבחירות לנשיאות: האם יו"ר הכנסת לוין ייכנס לנעלי ריבלין?',
-      img: `${process.env.PUBLIC_URL}/imges/event2.jpg`,
-    },
-    {
-      id: '3',
-      CardTitle: 'thirdone',
-      CardSubtitle: 'third',
-      date: '3thFeb',
-      body: 'הפלונטר והבחירות לנשיאות: האם יו"ר הכנסת לוין ייכנס לנעלי ריבלין?',
-      img: `${process.env.PUBLIC_URL}/imges/event3.jpg`,
-    },
-  ];
   <script src='https://code.iconify.design/1/1.0.7/iconify.min.js'></script>;
   return (
     <>

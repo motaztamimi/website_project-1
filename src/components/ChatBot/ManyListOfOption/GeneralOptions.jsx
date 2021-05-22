ef83efa9adaadb0d9de4507795cfef08d4f5b80a
motaztamimi <74739600+motaztamimi@users.noreply.github.com>
2021-05-22 13:11:10 +0300

    update some of things in Bot

--- src/components/ChatBot/ManyListOfOption/GeneralOptions.jsx
+++ src/components/ChatBot/ManyListOfOption/GeneralOptions.jsx
@@ -7,14 +7,19 @@ import Options from '../Opthion/Opthion';
 const GeneralOptions = (props) => {
   const opthions = [
     {
-      text: 'im first opthion',
+      text: 'מי אתם',
       handler: props.actionProvider.erorhandler,
       id: 1,
     },
     {
-      text: 'im first second',
+      text: 'במה האתר יכול לעזור לי',
       handler: props.actionProvider.listhandler,
       id: 2,
+    },
+    {
+      text: ' מה ההבדל בין האתר הישן לחדש',
+      handler: props.actionProvider.listhandler,
+      id: 3,
     },
   ];
   return <Options options={opthions} {...props} />;

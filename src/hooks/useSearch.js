af1f1a0584c9eba19aa34c07b3321c41af1a65a1
MustafaAbuGhannam <mustafa99au@gmail.com>
2021-06-02 16:51:25 +0300

    updated search functions

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -6,43 +6,21 @@ import useDatabase from './useDataBase';
 // this hook will retrive the nescassary data form the dataBase server then will start the search Functionality on the data
 // our search Functionality is naive that is beacause of time shortage and also on skill shortage in parsing Strings
 // our search Functionality works by asking if the given data string includes parts of the serachString given to the function
-const useSearch = (serachString) => {
-  let secToAdd = [];
-  const News = useDatabase('News');
-  const Researches = useDatabase('Researches');
-  const Departments = ['כפר שאול', 'איתנים', 'שירות קהילתי'];
-  const newsResults = [];
-  const [
-    DepartmentsFromDataBaseResults,
-    setDepartmentsFromDataBaseResults,
-  ] = useState([]);
-  const DepartmentsResults = [];
-  // cheacking if the serachString includes one of the 3 Departments if so push it to the results array
-  Departments.forEach((item) => {
+const useSearch = async () => {
+  return await dataBase.collection('News').get();
+};
+
+export default useSearch;
+
+// cheacking if the serachString includes one of the 3 Departments if so push it to the results array
+/*   Departments.forEach((item) => {
     if (item.includes(serachString)) {
       DepartmentsResults.push(item);
     }
-  });
-  const ResearchesResults = [];
+  }); */
 
-  // When reciving the data form the dataBase start the Serarch functionality
-
-  useEffect(() => {
-    if (News.docs.length !== 0) {
-      News.docs.forEach((item) => {
-        if (item.NewsBody.includes(serachString)) {
-          newsResults.push(item);
-        } else if (item.NewsSubTitle.includes(serachString)) {
-          newsResults.push(item);
-        } else if (item.NewsTitle.includes(serachString)) {
-          newsResults.push(item);
-        }
-      });
-    }
-  }, [News]);
-
-  // When reciving the data form the dataBase start the Serarch functionality
-  useEffect(() => {
+// When reciving the data form the dataBase start the Serarch functionality
+/*   useEffect(() => {
     if (Researches.docs.length !== 0) {
       Researches.docs.forEach((item) => {
         if (item.name.includes(serachString)) {
@@ -50,29 +28,37 @@ const useSearch = (serachString) => {
         }
       });
     }
-  }, [ResearchesResults]);
+  }, [ResearchesResults]); */
 
-  useEffect(() => {
+/* useEffect(() => {
     const collectionRef = dataBase.collection('Departments');
     collectionRef.get().then((value) => {
       value.docs.forEach((doc) => {
-        doc.data().Deps.forEach((dep, index) => {
-          if (dep.includes(serachString)) {
-            secToAdd = [...secToAdd, ...[`${doc.id}/${index}`]];
-            setDepartmentsFromDataBaseResults(secToAdd);
-          }
-        });
-        /// I need to countiue coding here
+        if (doc.id !== 'אחר') {
+          doc.data().Deps.forEach((dep, index) => {
+            if (dep.includes(serachString)) {
+              secToAdd = [...secToAdd, ...[`${doc.id}/${index}`]];
+              setDepartmentsFromDataBaseResults(secToAdd);
+            }
+          });
+        }
+      });
+      /// I need to countiue coding here
+      value.docs.forEach((doc) => {
+        if (doc.id !== 'אחר') {
+          doc.data().Deps.forEach((dep, index) => {
+            var data = doc.data();
+            if (data[dep] !== undefined) {
+              if (data[dep].first.includes(serachString)) {
+                contentSecToAdd.push({
+                  contentText: serachString,
+                  contentLocation: `${doc.id}/${index}`,
+                });
+                setContentSec(contentSecToAdd);
+              }
+            }
+          });
+        }
       });
     });
-  }, []);
-  // returning an object which includes all the result form the diffrent sections
-  return {
-    ResearchesResults,
-    newsResults,
-    DepartmentsResults,
-    DepartmentsFromDataBaseResults,
-  };
-};
-
-export default useSearch;
+  }, []); */
--- src/search/Search.jsx
+++ src/search/Search.jsx
@@ -1,9 +1,182 @@
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import useSearch from '../hooks/useSearch';
-
+import { dataBase } from '../config/firebase';
 const Search = () => {
-  const values = useSearch('מחלקה פעילה (סגורה) א׳');
-  console.log(values);
-  return <div>Hello world</div>;
+  const searchString = 'מחשבים, מתקני ספורט , טיפול משפחתי, טיפול באומנות ועוד';
+  const [newsFromSearch, setNewsFromSearch] = useState(null);
+  const [departmentsFromSearch, setDepartmentsFromSearch] = useState(null);
+  const sections = ['איתנים', 'כפר שאול', 'שירות קהילתי'];
+  const [contentSec, setContentSec] = useState(null);
+  var sectionsFromSearch = '';
+  var secToAdd = [];
+  var contentSecToAdd = [];
+
+  sections.forEach((section) => {
+    if (section.includes(searchString)) {
+      if (section === 'כפר שאול') {
+        sectionsFromSearch = section;
+      } else if (section === 'איתנים') {
+        sectionsFromSearch = section;
+      } else {
+        sectionsFromSearch = section;
+      }
+    }
+  });
+
+  useEffect(() => {
+    dataBase
+      .collection('News')
+      .get()
+      .then((news) => {
+        const toAdd = [];
+        news.docs.forEach((item) => {
+          let data = item.data();
+          let id = item.id;
+          data = { ...data, id };
+          if (data.NewsBody.includes(searchString)) {
+            toAdd.push(data);
+          } else if (data.NewsSubTitle.includes(searchString)) {
+            toAdd.push(data);
+          } else if (data.NewsTitle.includes(searchString)) {
+            toAdd.push(data);
+          }
+        });
+        setNewsFromSearch(toAdd);
+      });
+  }, []);
+
+  useEffect(() => {
+    const collectionRef = dataBase.collection('Departments');
+    collectionRef.get().then((value) => {
+      value.docs.forEach((doc) => {
+        if (doc.id !== 'אחר') {
+          doc.data().Deps.forEach((dep, index) => {
+            if (dep.includes(searchString)) {
+              let id = '';
+              if (doc.id === 'כפר שאול') {
+                id = 'kfarShaol';
+              } else if (doc.id === 'איתנים') {
+                id = 'Etanim';
+              } else {
+                id = 'sherotKhelate';
+              }
+              secToAdd = [
+                ...secToAdd,
+                ...[{ url: `${id}/${index}`, title: `${dep}/${doc.id}` }],
+              ];
+              setDepartmentsFromSearch(secToAdd);
+            }
+          });
+        }
+      });
+
+      value.docs.forEach((doc) => {
+        if (doc.id !== 'אחר') {
+          doc.data().Deps.forEach((dep, index) => {
+            var data = doc.data();
+            if (data[dep] !== undefined) {
+              if (data[dep].first && data[dep].first.includes(searchString)) {
+                let id = '';
+                if (doc.id === 'כפר שאול') {
+                  id = 'kfarShaol';
+                } else if (doc.id === 'אינתים') {
+                  id = 'Etanim';
+                } else {
+                  id = 'sherotKhelate';
+                }
+                contentSecToAdd.push({
+                  contentText: searchString,
+                  url: `${id}/${index}`,
+                });
+                setContentSec(contentSecToAdd);
+              }
+            }
+            if (data[dep] !== undefined) {
+              if (data[dep].second && data[dep].second.includes(searchString)) {
+                let id = '';
+                if (doc.id === 'כפר שאול') {
+                  id = 'kfarShaol';
+                } else if (doc.id === 'אינתים') {
+                  id = 'Etanim';
+                } else {
+                  id = 'sherotKhelate';
+                }
+                contentSecToAdd.push({
+                  contentText: searchString,
+                  url: `${id}/${index}`,
+                });
+                setContentSec(contentSecToAdd);
+              }
+            }
+            if (data[dep] !== undefined) {
+              if (data[dep].theird && data[dep].theird.includes(searchString)) {
+                let id = '';
+                if (doc.id === 'כפר שאול') {
+                  id = 'kfarShaol';
+                } else if (doc.id === 'אינתים') {
+                  id = 'Etanim';
+                } else {
+                  id = 'sherotKhelate';
+                }
+                contentSecToAdd.push({
+                  contentText: searchString,
+                  url: `${id}/${index}`,
+                });
+                setContentSec(contentSecToAdd);
+              }
+            }
+            if (data[dep] !== undefined) {
+              if (data[dep].fourth && data[dep].fourth.includes(searchString)) {
+                let id = '';
+                if (doc.id === 'כפר שאול') {
+                  id = 'kfarShaol';
+                } else if (doc.id === 'אינתים') {
+                  id = 'Etanim';
+                } else {
+                  id = 'sherotKhelate';
+                }
+                contentSecToAdd.push({
+                  contentText: searchString,
+                  url: `${id}/${index}`,
+                });
+                setContentSec(contentSecToAdd);
+              }
+            }
+          });
+        }
+      });
+    });
+  }, []);
+
+  console.log(departmentsFromSearch);
+  console.log(contentSec);
+
+  return (
+    <div>
+      <div className='NewsFormSearch'>
+        {newsFromSearch &&
+          newsFromSearch.map((newsItem) => {
+            return (
+              <Link to={`News/${newsItem.id}`} key={newsItem.id}>
+                <div className='NewsItemfromSearch'>
+                  <h1>{newsItem.NewsTitle}</h1>
+                  <img src={newsItem.NewsImage} alt='img' />
+                </div>
+              </Link>
+            );
+          })}
+      </div>
+
+      <div className='SectionsFromSearch'>
+        {sectionsFromSearch && (
+          <div className='SectionFromSearch'>
+            <h1>{sectionsFromSearch}</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Search;

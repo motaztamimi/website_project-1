64f1767a6ddb7f264d89f90ef721f10d3637b1ea
Mazen <mazen.qaissi88@gmail.com>
2021-06-08 12:17:36 +0300

    save slide show old and automatic

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,54 +1,93 @@
+/** @format */
 
-
-import React from 'react';
-import {
-  CarouselProvider,
-  Slider,
-  Slide,
-  ButtonBack,
-  ButtonNext,
-} from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
 import '../style/SlideShow.css';
 
+import { useState, useEffect } from 'react';
 import useDataBase from '../hooks/useDataBase';
 const SlideShow = ({ collection }) => {
-  const SlideShows = useDataBase(collection).docs;
+  //this props is the array that we will get from the server.
+  const [slidenum, setSlide] = useState(1);
+  const [firstrender, setfirstrender] = useState(false);
+  var photoArray = useDataBase(collection).docs;
 
- 
-  let count = 2;
-  
+  const [res, setRes] = useState([]);
+
+  useEffect(() => {
+    if (photoArray.length === 0) {
+      return;
+    }
+    setfirstrender(true);
+    if (photoArray.length % 2 === 1) {
+      photoArray.push(photoArray[0]);
+    }
+    const temp = [];
+    while (photoArray.length > 0) {
+      //split the array into twos.
+      const chunk = photoArray.splice(0, 2);
+      temp.push(chunk);
+    }
+    setfirstrender(true);
+    setRes(temp);
+  }, [photoArray]);
+
+  useEffect(() => {
+    if (firstrender) {
+      Showslide(1);
+      aoutomaticChange();
+    }
+    setfirstrender(false);
+  }, [firstrender]);
+
+  const Showslide = (n) => {
+    let i;
+    let slide = document.getElementsByClassName('myslide');
+    let a = Array.from(slide);
+    let curentindex = n;
+    if (curentindex > a.length) {
+      curentindex = 1;
+    }
+    if (curentindex <= 0) {
+      curentindex = a.length;
+    }
+    for (i = 0; i < a.length; i++) {
+      a[i].style.display = 'none';
+    }
+
+    a[curentindex - 1].style.display = 'block';
+    setSlide(curentindex);
+  };
+
+  function plusSlides(n) {
+    Showslide(slidenum + n);
+  }
+  const aoutomaticChange = () => {
+    setTimeout(() => {
+      const btn = document.getElementById('SlideShowNextButton');
+      btn.click();
+      aoutomaticChange();
+    }, 3000);
+  };
   return (
-    <CarouselProvider
-      naturalSlideWidth={16}
-      naturalSlideHeight={355}
-      totalSlides={SlideShows.length}
-      visibleSlides={count}
-      // touchEnabled={true}
-      isPlaying={true}
-      infinite={true}
-      step={2}
-      playDirection={'backward'}
-      interval={4000}
-      >
-      <div className='sliderr0 slideShow-container'>
-        <div className='Backk0'>
-          <ButtonBack className="prev">&#10094;</ButtonBack>
-        </div>
-        <div >
-          <Slider className="myslide">
-            {SlideShows.map((element) => (
-              <Slide index={element.id} key={element.id}>
-                <img src={element.url} alt='' />
-              </Slide>
-            ))}
-          </Slider>
-        </div>
-        <div className='nextt0'>
-          <ButtonNext className="next">&#10095;</ButtonNext>
-        </div>
-      </div>
-    </CarouselProvider>
+    <div className='slideShow-container'>
+      {res.map(function (element, i) {
+        return (
+          <div className='myslide' key={i} style={{ display: 'none' }}>
+            <div>
+              <img src={element[0].url} alt='' />
+              <img src={element[1].url} alt='' />
+            </div>
+          </div>
+        );
+      })}
+
+      <button className='prev' id="SlideShowNextButton" onClick={() => plusSlides(-1)}>
+        &#10094;
+      </button>
+      <button className='next' id="SlideShowNextButton" onClick={() => plusSlides(1)}>
+        &#10095;
+      </button>
+    </div>
   );
 };
+
 export default SlideShow;
